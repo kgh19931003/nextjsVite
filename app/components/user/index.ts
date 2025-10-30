ba8d757b034d8dfdd459d337ff58992df3fb68d3
kgh19931003 <sasaa3865@gmail.com>
2025-10-30 18:01:14 +0900

    포트폴리오 업데이트2

--- app/components/user/index.ts
+++ app/components/user/index.ts
@@ -1,65 +1,22 @@
 import Main from './Main';
-import BlogList from './blog/BlogList';
-import BlogView from './blog/BlogView';
-import BlogLists from './blogs/BlogList';
-import BlogViews from './blogs/BlogView';
-import Business from './business/Business';
-import CompanyLocations from './company/Locations';
-import CompanyTimeline from './company/Timeline';
-import CompanyVision from './company/Vision';
-import CompanyCertification from './company/Certification';
-import Inquiry from '@/components/user/inquiry/Inquiry';
 import Privacy from './etc/Privacy';
 import EmailPolicy from './etc/EmailPolicy';
 import Term from './etc/Term';
 import PolicyPrivacy from './policy/Privacy';
 import PolicyEmailPolicy from './policy/EmailPolicy';
 import PolicyTerm from './policy/Term';
-import ServiceThreeDsPrinting from './service/ThreeDsPrinting';
-import ServiceRepair from './service/Repair';
-import ServiceDesign from './service/Design';
-import ServiceThreeDsScan from './service/ThreeDsScan';
-import Product from './product/Product';
-import BusinessManufacturing from './business/Manufacturing';
-import BusinessRepair from './business/Repair';
-import BusinessMetalpowder from './business/Metalpowder';
-import Powder from './powder/AlloyIntro';
-import PowderNiAlloy from './powder/NiAlloy';
-import PowderStainless from './powder/Stainless';
 import PolicyForm from './policy/Form';
 import PayTest from '@/components/user/pay/test';
 import PaySuccess from '@/components/user/pay/success';
 
 export {
     Main,
-    BlogList,
-    BlogView,
-    BlogLists,
-    BlogViews,
-    Business,
-    CompanyLocations,
-    CompanyTimeline,
-    CompanyVision,
-    CompanyCertification,
-    Inquiry,
     Privacy,
     EmailPolicy,
     Term,
     PolicyPrivacy,
     PolicyEmailPolicy,
     PolicyTerm,
-    ServiceThreeDsPrinting,
-    ServiceRepair,
-    ServiceDesign,
-    ServiceThreeDsScan,
-    Product,
-    BusinessManufacturing,
-    BusinessRepair,
-    BusinessMetalpowder,
-
-    Powder,
-    PowderNiAlloy,
-    PowderStainless,
 
     PolicyForm,
 
